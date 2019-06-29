b7e06b25cb5984fd36149fd487d2bb0547aa293f
Luke Peavey <1williampeavey@gmail.com>
2019-06-29 17:57:01 -0400

    Fix bug: GET /random occasionally returns 500 error
    Issue:
    The logs show that the /random endpoint occasionally responds with a
    500 error (roughly 1 every few thousand requests)
    
    Fix:
    The max value for the random index should be `documentCount - 1`

--- src/routes.js
+++ src/routes.js
@@ -16,13 +16,13 @@ const router = Router()
 router.get('/random', async (req, res, next) => {
   try {
     const documentCount = await Quotes.estimatedDocumentCount()
-    const index = random(0, documentCount)
+    const index = random(0, documentCount - 1)
     const entry = await Quotes.find({})
       .limit(1)
       .skip(index)
       .select('content author')
     if (!entry || !entry[0]) {
-      return next(createError(500))
+      return next(createError(500, `Invalid random index: \`${index}\``))
     }
     res.status(200).json(entry[0])
   } catch (error) {
