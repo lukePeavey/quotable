14537a519965c3388fc4fd1b8101b90e7ad4ef2e
Luke Peavey <peaveycodes@gmail.com>
2019-12-24 04:01:29 -0500

    Redirect root URL to github repository (#12)
    Until the website is complete, the root URL https://api.quotable.io/
    will redirect to the github repository page.

--- src/app.js
+++ src/app.js
@@ -9,6 +9,12 @@ const app = express()
 app.use(cors())
 app.use(moesifMiddleware())
 app.get('/favicon.ico', (req, res) => res.sendStatus(204))
+
+// Redirect the root URL to the github repository
+app.get('/', (req, res) => {
+  res.redirect('https://github.com/lukePeavey/quotable')
+})
+
 app.use(routes)
 app.use(handle404)
 app.use(logErrors)
