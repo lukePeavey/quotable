e97cce0cf9400b709cce9acc53d75b85f02b5a45
Luke Peavey <1williampeavey@gmail.com>
2020-01-31 11:56:06 -0500

    Refactor randomQuote controller
    Add a $match stage to aggregation pipeline to support filter params

--- src/controllers/quotes/randomQuote.js
+++ src/controllers/quotes/randomQuote.js
@@ -6,14 +6,23 @@ const Quotes = require('../../models/Quotes')
  */
 module.exports = async function getRandomQuote(req, res, next) {
   try {
-    // Use $sample to select a random document from Quotes
-    const results = await Quotes.aggregate([{ $sample: { size: 1 } }])
+    // Query filters
+    const filter = {}
 
-    res.status(200).json({
-      _id: results[0]._id,
-      content: results[0].content,
-      author: results[0].author,
-    })
+    const [result] = await Quotes.aggregate([
+      // Apply filters (if any)
+      { $match: filter },
+      // Select a random document from the results
+      { $sample: { size: 1 } },
+      // Only include the following the fields
+      { $project: { _id: 1, content: 1, author: 1 } },
+    ])
+
+    if (!result) {
+      // This should only occur when using filter params
+      return next(createError(404, 'Could not find any matching quotes'))
+    }
+    res.status(200).json(result)
   } catch (error) {
     return next(error)
   }
