aacf6401d7fa8ae3aa807e36d9095569c756e934
Luke Peavey <1williampeavey@gmail.com>
2021-03-13 23:09:38 -0500

    enhancement: add description field to authors
    Adds a description field to authors. This field is a short
    one-line description of the author, usually the person's
    nationality and primary occupation. For example:
    "Austrian physicist" or "American author".

--- src/models/Authors.js
+++ src/models/Authors.js
@@ -3,18 +3,20 @@ const shortid = require('shortid')
 
 const AuthorSchema = new Schema({
   _id: { type: String, default: shortid.generate },
+  // Slug can be used instead of `_id` to identify an author.
+  slug: { type: String, required: true },
   // The authors full name
   name: { type: String, required: true },
   // A list of alternate names (for internal use)
   aka: { type: [String], default: [] },
   // The link to the authors WikiPedia page or website
   link: { type: String, default: '' },
-  // A short description about the person
+  // A brief biography of the author (typically 2 to 3 sentences)
   bio: { type: String, default: '' },
+  // A short, one-line description of the author (typically 1 to 3 words)
+  description: { type: String, default: '' },
   // The number of quotes by this author
   quoteCount: { type: Number, required: true },
-  // URL safe version of the author name
-  slug: { type: String, required: true },
 })
 
 AuthorSchema.set('collation', { locale: 'en_US', strength: 1 })
